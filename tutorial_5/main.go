800e2c055290b13fa8408d7596a67bc276b7a01c
Kapil Bavisiya <kapilbavisiya555@gmail.com>
2020-11-07 19:32:05 +0530

    updated

---
+++ tutorial_5/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// Book struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// Init books variable  as a slice Book struct
+var books []Book
+
+// Get all books 📚
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// Get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	params := mux.Vars(r) // Get params
+	// Loop thorugh books and find with ID
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+// Create a new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000)) // Mock id - not safe
+	books = append(books, book)
+
+	json.NewEncoder(w).Encode(book)
+
+}
+
+// update book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+}
+
+// delete book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+
+	}
+	json.NewEncoder(w).Encode(books)
+
+}
+
+func main() {
+	// Inti router
+	r := mux.NewRouter()
+
+	// Mock data - @totdo - implement database
+	books = append(books, Book{ID: "1", Isbn: "4585121", Title: "Think and Grow Rich", Author: &Author{Firstname: "Napoelian", Lastname: "hill"}})
+	books = append(books, Book{ID: "2", Isbn: "5466121", Title: "How to win frinds", Author: &Author{Firstname: "Brian", Lastname: "tracy"}})
+
+	// Route handlers/ Endpoints
+
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
