7bfe8f8ef77304800542ed30b51c75d3e71e479d
Kapil bavisiya <kapilbavisiya555@gmail.com>
2020-12-16 16:28:18 +0530

    assignment-6

--- assignment-3/product/main.go
+++ assignment-3/product/main.go
@@ -12,18 +12,13 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-// Order represents the model for an order
-// Default table name will be `orders`
 type Order struct {
-	// gorm.Model
 	OrderID      uint   `json:"orderId" gorm:"primary_key"`
 	CustomerName string `json:"customerName"`
 	Items        []Item `json:"items" gorm:"foreignkey:OrderID"`
 }
 
-// Item represents the model for an item in the order
 type Item struct {
-	// gorm.Model
 	LineItemID  uint   `json:"lineItemId" gorm:"primary_key"`
 	ItemCode    string `json:"itemCode"`
 	Description string `json:"description"`
@@ -43,19 +38,14 @@ func initDB() {
 		panic("failed to connect database")
 	}
 
-	// Create the database. This is a one-time step.
-	// Comment out if running multiple times - You may see an error otherwise
 	// db.Exec("CREATE DATABASE order_db")
 	db.Exec("USE order_db")
-
-	// Migration to create tables for Order and Item schema
 	db.AutoMigrate(&Order{}, &Item{})
 }
 
 func createOrder(w http.ResponseWriter, r *http.Request) {
 	var order Order
 	json.NewDecoder(r.Body).Decode(&order)
-	// Creates new order by inserting records in the `orders` and `items` table
 	db.Create(&order)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(order)
@@ -90,9 +80,7 @@ func updateOrder(w http.ResponseWriter, r *http.Request) {
 func deleteOrder(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	inputOrderID := params["orderId"]
-	// Convert `orderId` string param to uint64
 	id64, _ := strconv.ParseUint(inputOrderID, 10, 64)
-	// Convert uint64 to uint
 	idToDelete := uint(id64)
 
 	db.Where("order_id = ?", idToDelete).Delete(&Item{})
@@ -102,18 +90,12 @@ func deleteOrder(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
-	// Create
-	router.HandleFunc("/orders", createOrder).Methods("POST")
-	// Read
-	router.HandleFunc("/orders/{orderId}", getOrder).Methods("GET")
-	// Read-all
 	router.HandleFunc("/orders", getOrders).Methods("GET")
-	// Update
+	router.HandleFunc("/orders", createOrder).Methods("POST")
+	router.HandleFunc("/orders/{orderId}", getOrder).Methods("GET")
 	router.HandleFunc("/orders/{orderId}", updateOrder).Methods("PUT")
-	// Delete
 	router.HandleFunc("/orders/{orderId}", deleteOrder).Methods("DELETE")
-	// Initialize db connection
-	initDB()
 
+	initDB()
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
